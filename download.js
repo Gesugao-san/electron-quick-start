73969a7722a1d7ba9de92a66998af0e0bba40db0
Gesugao-san <gesugao.san@gmail.com>
2022-09-12 00:19:36 +0300

    Add feed getting and processing to <ul> tag

--- download.js
+++ download.js
@@ -25,76 +25,159 @@ let data_matrix = {
 			"meta": {
 				"filename": path.join("cache", "feed")
 			},
-			"raw": {
-				"exist": false,
-				"path": path.join(cwd, "cache", "feed.js")
-			},
 			"processed": {
 				"exist": false,
-				"path": null
+				"path": path.join(cwd, "cache", "feed.json")
 			},
 		}
 	}
 }
 
+let feed_data = {};
 
-function status_update(data) {
-  document.getElementById("download_status").innerHTML = data;
+// https://stackoverflow.com/a/45605555/8175291
+function createList(mode, data = Array()) {
+  console.log('createList mode:', mode);
+  switch (mode) {
+    case 'feed':
+      const feeds_arr = data; //feed_data.pagerinit_dict.lists.feed;
+      console.log('feeds_arr:', feeds_arr);
+      for (let i = 0; i < feeds_arr.length; i++) {
+        const current_feed = feeds_arr[i];
+        console.log('current_feed:', current_feed);
+        var listView = document.createElement('ul');
+        listView.id = current_feed.post;
+        for (const [key, value] of Object.entries(current_feed)) {
+          var listViewItem = document.createElement('li');
+          if (key == 'web_url') {
+            var link = document.createElement("a");
+            link.setAttribute('href', value);
+            link.setAttribute('target', '_blank');
+            link.textContent = value;
+            console.log('link:', link);
+            listViewItem.appendChild(link);
+          } else {
+            listViewItem.appendChild(document.createTextNode(`${key}: ${value}`)); //current_feed[ii]
+          }
+          listView.appendChild(listViewItem);
+        }
+        /* for (var ii = 0; ii < Object.keys(current_feed).length; ii++) { } */
+      }
+      return listView; //break;
+    default:
+      var listView = document.createElement('ul');
+      for (var i = 0; i < data.length; i++) {
+        var listViewItem = document.createElement('li');
+        listViewItem.appendChild(document.createTextNode(data[i]));
+        listView.appendChild(listViewItem);
+      }
+      return listView; //break;
+  }
 }
 
-function doNotify(evt) {
-  //console.debug('Debug:', evt);
-  if (evt.srcElement.id == "do_download") {
-    status_update('Download started...'); //"You click on me!"
-    const user_ip = "You";
-    console.log(`${user_ip} pulls feed.`);
-    const file = fs.createWriteStream(data_matrix.cache.feed.raw.path);
-    console.log(`Debug: ${host}:${port}/openb/get/feed.js?lorem=ipsum`);
-    const req_file = http.get(`${host}:${port}/openb/get/feed.js?lorem=ipsum`, (response) => {
-      console.log(`${user_ip}: Download started.`);
-      response.pipe(file);
-      // after download completed close filestream
-      file.on('finish', () => {
-        console.log(`${user_ip}: Download completed.`);
-        status_update('Download completed');
-        file.close();
+function status_update(element, data) {
+  document.getElementById(element).innerHTML = data;
+  return;
+}
+
+function func_feed_download(ev) {
+  switch (ev.srcElement.id) {
+    case "feed_download_clear":  // if (ev.srcElement.id == "feed_download_action") {
+      fs.unlink(data_matrix.cache.feed.processed.path, (error) => {
+        if (error) console.error(error);
       });
-      file.once('close', () => {
-        try {
-          stat = fs.statSync(data_matrix.cache.feed.raw.path);
-          /* fs.rename(data_matrix.cache.feed.raw.path, data_matrix.cache.feed.raw.path, (error) => {
-            if (error) {
-              console.error(error);
-              res.writeHead(500); // res.statusCode = 500;
-              res.end(JSON.stringify({error: "Internal Server Error!", details: error}));
-            }
-            console.log(`${user_ip}: File raw.`);
-          }); */
-          // https://stackoverflow.com/a/17699926/8175291
-          /* fs.stat('foo.txt', (err, stat) => {
-            if (err == null) {
-              console.log('File exists');
-            } else if (err.code === 'ENOENT') {
-              console.log('file does not exist');
-            } else {
-              console.log('Some other error: ', err.code);
-            }
-          }); */
-          //res.writeHead(200); //res.statusCode = 200;
-          //res.end(JSON.stringify({message: "Update done!", rtime: timestamp.valueOf()}));
-          status_update('200');
-        }
-        catch (err) {
-          console.log(err);
-          status_update(err);
-          //res.writeHead(500); //res.statusCode = 500;
-          //res.end(JSON.stringify({error: "Internal Server Error!", details: err}));
-        }
+      feed_data = {};
+      status_update('feed_download_status', 'Feed file cleared.');
+      break;
+    case "feed_download_action":
+      status_update('feed_download_status', 'Feed file download started...'); //"You click on me!"
+      const user_ip = "You";
+      console.log(`${user_ip} pulls feed.`);
+      const file = fs.createWriteStream(data_matrix.cache.feed.processed.path);
+      console.log(`Debug (URL): ${host}:${port}/openb/get/feed.json?lorem=ipsum`);
+      const req_file = http.get(`${host}:${port}/openb/get/feed.json?lorem=ipsum`, (response) => {
+        console.log(`${user_ip}: Download started.`);
+        response.pipe(file);
+        // after download completed close filestream
+        file.on('finish', () => {
+          console.log(`${user_ip}: Download completed.`);
+          status_update('feed_download_status', 'Feed file download completed.');
+          file.close();
+        });
+        file.once('close', () => {
+          try {
+            stat = fs.statSync(data_matrix.cache.feed.processed.path);
+            /* fs.rename(data_matrix.cache.feed.processed.path, data_matrix.cache.feed.processed.path, (error) => {
+              if (error) {
+                console.error(error);
+                res.writeHead(500); // res.statusCode = 500;
+                res.end(JSON.stringify({error: "Internal Server Error!", details: error}));
+              }
+              console.log(`${user_ip}: File processed.`);
+            }); */
+            // https://stackoverflow.com/a/17699926/8175291
+            /* fs.stat('foo.txt', (err, stat) => {
+              if (err == null) {
+                console.log('File exists');
+              } else if (err.code === 'ENOENT') {
+                console.log('file does not exist');
+              } else {
+                console.log('Some other error: ', err.code);
+              }
+            }); */
+            //res.writeHead(200); //res.statusCode = 200;
+            //res.end(JSON.stringify({message: "Update done!", rtime: timestamp.valueOf()}));
+            status_update('feed_download_status', '200');
+          }
+          catch (err) {
+            console.log(err);
+            status_update('feed_download_status', err);
+            //res.writeHead(500); //res.statusCode = 500;
+            //res.end(JSON.stringify({error: "Internal Server Error!", details: err}));
+          }
+        });
       });
-    });
+      break;
+    case "feed_download_read":  // if (ev.srcElement.id == "feed_download_action") {
+      feed_data = require(`./${data_matrix.cache.feed.meta.filename}`);
+      console.log('feed_data:', feed_data);
+      console.log('Last feed update (Unix Timestamp; Date):', feed_data.feedupdate_dict.date);
+      status_update('feed_download_status', 'Feed file readed.');
+      break;
+    default:
+      console.debug('Debug (event):', ev);
+      break;
   }
+  return;
 };
 
+function func_feed_process(ev) {
+  let feeds_shown = document.getElementById('feeds_shown');
+  switch (ev.srcElement.id) {
+    case "feed_process_clear":
+      status_update('process_status', 'Feed cleared.');
+      feeds_shown.innerHTML = "";
+      break;
+    case "feed_process_action":
+      status_update('process_status', 'Feed processed.');
+      func_feed_download({srcElement: {id: 'feed_download_read'}});
+      //feeds_shown.appendChild(createList([1, 2, 3]));
+      const list = createList('feed', feed_data.pagerinit_dict.lists.feed);
+      console.log('list:', list);
+      feeds_shown.appendChild(list);
+      break;
+    default:
+      console.debug('Debug (event):', ev);
+      break;
+    }
+  return;
+};
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById("do_download").addEventListener("click", doNotify);
+  document.getElementById("feed_download_clear").addEventListener("click", func_feed_download);
+  document.getElementById("feed_download_action").addEventListener("click", func_feed_download);
+  document.getElementById("feed_download_read").addEventListener("click", func_feed_download);
+  document.getElementById("feed_process_action").addEventListener("click", func_feed_process);
+  document.getElementById("feed_process_clear").addEventListener("click", func_feed_process);
 });
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, shell} = require('electron');
+const path = require('path');
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -16,22 +16,27 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   })
 })
 
@@ -39,7 +44,7 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit();
 })
 
 // In this file you can include the rest of your app's specific main process
