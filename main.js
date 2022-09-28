5d71cf1519ecd01a0b955b76b924c2ef348db864
Gesugao-san <gesugao.san@gmail.com>
2022-09-28 18:48:50 +0300

    Experimenting with creations.html

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow () {
       enableRemoteModule: true,
       preload: path.join(__dirname, 'src/backend/electron/preload.js')
     }
-  })
+  });
 
   // and load the index.html of the app.
   mainWindow.loadFile('src/frontend/index.html');
@@ -34,19 +34,19 @@ function createWindow () {
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', function () {
+  app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  })
-})
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
---
+++ src/playground/creations.js
@@ -0,0 +1,111 @@
+
+
+const data = {
+  "rtime": 1663653482, // Tue Sep 20 2022 05:58:02 GMT+0000 (http://unixtimestamp.com)
+  "type": "hub",
+  "author.title": {
+    "id": 0,
+    "hidden": true,
+    "hubpath": "author.title",
+    "title": "title",
+    "author": "author",
+    "type": "type",
+    "play_mode": "play_mode",
+    "score": 0,
+    "has_benefits": 1,
+    "icon": "./creation.png", // "id.png"
+    "smallicon": "id_s.png",
+    "download": true,
+    "ziplen": 0,
+    "list": "list",
+    "fans": 0,
+    "tags": null,
+    "date": 1663653482,
+    "mtime": 1663653482,
+    "rtime": 1663653482
+  }
+};
+
+
+/* https://stackoverflow.com/a/2324826/8175291 */
+function appendDataToTable(force_overwrite = false, do_clear = false, /* do_not_rebuild = false */) {
+  //console.log('Func was called.');
+  let creations_table, tables, icon, data_langing, inserting_data, creations_table_exist = false;
+  let cell, caption, row, tbody;
+  creations_table = document.getElementById('creations_table');
+  if (!creations_table || force_overwrite) {
+    if (do_clear) return console.log('Table already cleared, passing.');
+    console.log((force_overwrite ? 'Forced to rec' : 'C') + 'reating table...');
+    creations_table = document.createElement('table');
+    creations_table.id = 'creations_table';
+    caption = creations_table.createCaption();
+    caption.textContent = 'Creations';
+    //creations_table.createTHead();
+    tbody = creations_table.createTBody();
+    row = tbody.insertRow(0);
+    cell = row.insertCell(0);
+    icon = document.createElement('img');
+    icon.src = "./creation.png";
+    icon.style = "display: inline";
+    cell.appendChild(icon); //document.createTextNode(String(template)));
+    data_langing = document.createElement('div');
+    data_langing.style = "display: inline";
+    for (const key of ['rtime']) {
+      //console.log('key:', key, data[key]);
+      inserting_data = document.createElement('div');
+      inserting_data.id = key;
+      inserting_data.style = "display: inline";
+      inserting_data.appendChild(document.createTextNode(String(data[key])));
+      inserting_data.appendChild(document.createElement('br'));
+      data_langing.appendChild(inserting_data); //document.createTextNode(String(template)));
+    }
+    for (const key of ['id', 'hubpath']) {
+      //console.log('key:', key, data['author.title'][key]);
+      inserting_data = document.createElement('div');
+      inserting_data.id = key;
+      inserting_data.style = "display: inline";
+      inserting_data.appendChild(document.createTextNode(String(data['author.title'][key])));
+      data_langing.appendChild(document.createTextNode(String(' ')));
+      data_langing.appendChild(inserting_data);
+    }
+    cell.appendChild(data_langing); //document.createTextNode(String(template)));
+    console.log('Created table.');
+  } else {
+    creations_table_exist = true;
+    tbody = creations_table.getElementsByTagName('tbody')[0];
+  }
+  if (!creations_table_exist && !do_clear && !force_overwrite) {
+    tables = document.getElementById('tables-landing');
+    tables.appendChild(creations_table);
+    console.log('Table landed:', creations_table);
+    return;
+  } else if (do_clear) {
+    tables = document.getElementById('tables-landing');
+    let creations_table2 = document.getElementById('creations_table');
+    tables.removeChild(creations_table2);
+    console.log('Table removed.');
+    return;
+  } else {
+    tables = document.getElementById('tables-landing');
+    let creations_table2 = document.getElementById('creations_table');
+    if (creations_table2) {
+      if ((creations_table.outerHTML == creations_table2.outerHTML) && !force_overwrite)
+        return console.log('Existing and inserting tables are identical, passing.');
+      tables.removeChild(creations_table2);
+    }
+    tables.appendChild(creations_table); // tables.insertBefore(creations_table, creations_table);
+    console.log('Table rewrited.');
+    return;
+  }
+}
+
+
+function debug_damage() {
+  let creations_table, tables, icon, data_langing, inserting_data, creations_table_exist = false;
+  let cell, caption, row, tbody;
+  creations_table = document.getElementById('creations_table');
+  if (!creations_table) return console.log('Table to damage not existing, passing.');
+  creations_table.deleteCaption();
+  console.log('Damage: ok');
+}
+
